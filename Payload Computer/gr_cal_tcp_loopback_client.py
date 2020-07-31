af31a7ab3b031b60fac604d70fb868b0d123b665
km5es <krishnamakhija@gmail.com>
2020-07-31 19:16:30 -0400

    payload sampling rate is now 30.72 MSPS.

--- Payload Computer/gr_cal_tcp_loopback_client.py
+++ Payload Computer/gr_cal_tcp_loopback_client.py
@@ -5,7 +5,7 @@
 # Title: gr_cal_tcp_loopback_client
 # Author: KM
 # Description: This will go on the drone. A predefined waveform is fed into the companion script which creates a TCP server and loops back into this script. The server also checks for serial toggle and triggers GPIO at set points.
-# Generated: Tue Jul 28 21:43:52 2020
+# Generated: Fri Jul 31 19:15:54 2020
 ##################################################
 
 from gnuradio import blocks
@@ -32,7 +32,7 @@ class gr_cal_tcp_loopback_client(gr.top_block):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 7.68e6*2
+        self.samp_rate = samp_rate = 7.68e6*4
         self.min_buffer = min_buffer = 16*4096
         self.freq = freq = 150e6
 
