e1e36b843f5e14e7b1289707918f5cc1e23660f2
km5es <krishnamakhija@gmail.com>
2020-08-18 23:12:16 -0400

    testing ZMQ on the raspberry pi with Python 3.0 and GNU Radio 3.8. Exciting.

--- Payload Computer/gr_cal_tcp_loopback_client.py
+++ Payload Computer/gr_cal_tcp_loopback_client.py
@@ -1,33 +1,30 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-##################################################
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
 # GNU Radio Python Flow Graph
 # Title: gr_cal_tcp_loopback_client
 # Author: KM
 # Description: This will go on the drone. A predefined waveform is fed into the companion script which creates a TCP server and loops back into this script. The server also checks for serial toggle and triggers GPIO at set points.
-# Generated: Fri Jul 31 20:51:40 2020
-##################################################
+# GNU Radio version: 3.8.1.0
 
-from gnuradio import blocks
-from gnuradio import eng_notation
 from gnuradio import gr
-from gnuradio import uhd
-from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from grc_gnuradio import blks2 as grc_blks2
-from optparse import OptionParser
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import uhd
 import time
-
+from gnuradio import zeromq
 
 class gr_cal_tcp_loopback_client(gr.top_block):
 
-    def __init__(self, device_transport='send_frame_size=8192, num_send_frames=512'):
+    def __init__(self):
         gr.top_block.__init__(self, "gr_cal_tcp_loopback_client")
-
-        ##################################################
-        # Parameters
-        ##################################################
-        self.device_transport = device_transport
 
         ##################################################
         # Variables
@@ -39,40 +36,29 @@ class gr_cal_tcp_loopback_client(gr.top_block):
         ##################################################
         # Blocks
         ##################################################
+        self.zeromq_sub_source_0 = zeromq.sub_source(gr.sizeof_gr_complex, 1, 'tcp://127.0.0.1:8810', 100, False, -1)
+        self.zeromq_sub_source_0.set_min_output_buffer(65536)
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
-        	",".join((device_transport, "")),
-        	uhd.stream_args(
-        		cpu_format="fc32",
-        		channels=range(1),
-        	),
+            ",".join(("", "")),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+            '',
         )
         self.uhd_usrp_sink_0.set_clock_source('external', 0)
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
         self.uhd_usrp_sink_0.set_center_freq(freq, 0)
         self.uhd_usrp_sink_0.set_gain(20, 0)
-        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, min_buffer)
-        (self.blocks_vector_to_stream_0).set_min_output_buffer(65536)
-        self.blks2_tcp_source_0 = grc_blks2.tcp_source(
-        	itemsize=gr.sizeof_gr_complex*min_buffer,
-        	addr='127.0.0.1',
-        	port=8810,
-        	server=False,
-        )
-        (self.blks2_tcp_source_0).set_min_output_buffer(65536)
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec())
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blks2_tcp_source_0, 0), (self.blocks_vector_to_stream_0, 0))
-        self.connect((self.blocks_vector_to_stream_0, 0), (self.uhd_usrp_sink_0, 0))
-
-    def get_device_transport(self):
-        return self.device_transport
-
-    def set_device_transport(self, device_transport):
-        self.device_transport = device_transport
+        self.connect((self.zeromq_sub_source_0, 0), (self.uhd_usrp_sink_0, 0))
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -95,22 +81,20 @@ class gr_cal_tcp_loopback_client(gr.top_block):
         self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
 
 
-def argument_parser():
-    description = 'This will go on the drone. A predefined waveform is fed into the companion script which creates a TCP server and loops back into this script. The server also checks for serial toggle and triggers GPIO at set points.'
-    parser = OptionParser(usage="%prog: [options]", option_class=eng_option, description=description)
-    parser.add_option(
-        "", "--device-transport", dest="device_transport", type="string", default='send_frame_size=8192, num_send_frames=512',
-        help="Set device_transport [default=%default]")
-    return parser
-
 
 def main(top_block_cls=gr_cal_tcp_loopback_client, options=None):
-    if options is None:
-        options, _ = argument_parser().parse_args()
     if gr.enable_realtime_scheduling() != gr.RT_OK:
-        print "Error: failed to enable real-time scheduling."
+        print("Error: failed to enable real-time scheduling.")
+    tb = top_block_cls()
 
-    tb = top_block_cls(device_transport=options.device_transport)
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
     tb.start()
     tb.wait()
 
