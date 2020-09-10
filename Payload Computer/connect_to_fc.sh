f8d68e3354d0aba4bd230ef1227f8ed563028f53
km5es <krishnamakhija@gmail.com>
2020-09-10 05:52:55 -0400

    fixing bugs in shell script for fc connect.

--- Payload Computer/connect_to_fc.sh
+++ Payload Computer/connect_to_fc.sh
@@ -4,12 +4,12 @@
 ###### date: Sep 9th 2020
 
 ### wait for FC to boot up
-sleep 8;
+sleep 1;
 
 ### start MAVROS
 roslaunch mavros px4.launch fcu_url:="/dev/ttyACM0:57600" &
 
-sleep 5;
+sleep 8;
 
 ### start get_metadata.py
 gnome-terminal -e "bash -c 'python get_metadata.py;'"
