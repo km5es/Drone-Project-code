047bc05ceeabaab8274530967e7873be4e689ef8
km5es <krishnamakhija@gmail.com>
2020-09-11 03:38:41 -0400

    fixing bugs in shell scripts for payload.

--- Payload Computer/connect_to_fc.sh
+++ Payload Computer/connect_to_fc.sh
@@ -2,10 +2,10 @@
 #### Used within cron job to start MAVROS and get_metadata.py
 ##### author: Krishna Makhija
 ###### date: Sep 9th 2020
+source ~/.bashrc
 
 ### wait for FC to boot up
-sleep 1;
-source ~/.zshrc
+sleep 8;
 
 ### start MAVROS
 roslaunch mavros px4.launch fcu_url:="/dev/ttyACM0:57600" &
