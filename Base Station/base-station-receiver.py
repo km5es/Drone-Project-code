055dd67abb1e30db61563c45e8183c30c7040d16
km5es <krishnamakhija@gmail.com>
2020-09-11 05:28:43 -0400

    experimenting with logs and gitignore.

--- Base Station/base-station-receiver.py
+++ Base Station/base-station-receiver.py
@@ -83,57 +83,6 @@ def reset_buffer():
     ser.reset_output_buffer()
 
 
-def callback_local(data):
-    """
-    Callback object for drone's local position.
-    """
-    file.write("\t\t%s\t%s\t%s" % (data.pose.position.x,
-                                   data.pose.position.y, data.pose.position.z))
-    rospy.sleep(0.2)
-
-
-def callback_setpoint(data):
-    """
-    Callback object for drone's  setpoint position.
-    """
-    file.write("\t\t\t\t\t%s\t%s\t%s\n" %
-               (data.position.x, data.position.y, data.position.z))
-    rospy.sleep(0.2)
-
-
-def callback_SDR(data):
-    """
-    Callback object for SDR temperature.
-    """
-    current_time = time.strftime("%H%M%S-%d%m%Y")
-    file.write("%s\t%s\t" % (current_time, data))
-    rospy.sleep(0.2)
-
-
-def get_metadata():
-    """
-    ROS listener node for IMU data.
-    """
-    file.write("Timestamp\tTemperature\tLocal Position (x)\tLocal Position (y)\tLocal Position (z)\tSetpoint (x)\tSetpoint (y)\tSetpoint (z)\n")
-    rospy.init_node('get_metadata', anonymous=True)
-    while True:
-        if acq_event.is_set():
-            print(colored('Saving metadata in ' +
-                          str(metadata), 'grey', 'on_white'))
-            start = time.time()
-            start_timeout = start + timeout
-            rospy.Subscriber('sdr_temperature', Float32, callback_SDR)
-            rospy.Subscriber('/mavros/local_position/pose',
-                             PoseStamped, callback_local)
-            rospy.Subscriber('/mavros/setpoint_raw/target_local',
-                             PositionTarget, callback_setpoint)
-            rospy.spin()
-            if acq_event.is_set() == False:
-                break
-            elif time.time() > start_timeout:
-                break
-
-
 def recv_data():
     '''
     Wait for acq_event to begin and stop saving data.
