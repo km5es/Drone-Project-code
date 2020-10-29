3f3a11410a15f626fb2b6960a2af72c401f00a6c
km5es <krishnamakhija@gmail.com>
2020-10-29 01:49:34 -0400

    first commit in br-wifi. changed connect_to_fc.sh to include mavlink-router and tcp loopback.

--- Payload Computer/connect_to_fc.sh
+++ Payload Computer/connect_to_fc.sh
@@ -8,8 +8,11 @@ source /opt/ros/melodic/setup.bash
 ### wait for FC to boot up
 sleep 8;
 
+### route MAVLink data 
+mavlink-routerd -e 127.0.0.1:14550 -e 10.42.1.1:14550 -e 10.42.1.1:14551 /dev/ttyFC:921600 &
+
 ### start MAVROS
-roslaunch mavros px4.launch fcu_url:="/dev/ttyFC:921600" &
+roslaunch mavros px4.launch fcu_url:="udp://:14550@127.0.0.1:14550" &
 
 sleep 8;
 
