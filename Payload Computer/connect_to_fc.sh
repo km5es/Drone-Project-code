e213c2ad2e9324b4f9780133d3812a1a3d265113
Krishna <km5es@virginia.edu>
2020-12-26 04:53:34 -0500

    shell scripts are now MORE agnostic to ROS version on the payload computer and work better with cron jobs.

--- Payload Computer/connect_to_fc.sh
+++ Payload Computer/connect_to_fc.sh
@@ -2,8 +2,8 @@
 #### Used within cron job to start MAVROS and get_metadata.py
 ##### author: Krishna Makhija
 ###### date: Sep 9th 2020
-source ~/.bashrc
-source /opt/ros/$(rosversion -d)/setup.bash
+bash -c 'source ~/.bashrc'
+bash -c 'source /opt/ros/$(rosversion -d)/setup.bash'
 
 ### wait for FC to boot up
 sleep 8;
