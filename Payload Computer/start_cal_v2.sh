d1a98132b5a71f655461d63c378fdcfde3ab8098
km5es <krishnamakhija@gmail.com>
2020-12-26 19:34:03 -0500

    forgot to change start_cal_v2.sh

--- Payload Computer/start_cal_v2.sh
+++ Payload Computer/start_cal_v2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #kill -9 $(fuser /dev/ttyUSB0)
-bash -c 'source ~/.bashrc'
-bash -c 'source /opt/ros/$(rosversion -d)/setup.bash'
+source ~/.bashrc
+source /opt/ros/melodic/setup.bash
 
 lsof -t -i tcp:8810 | xargs kill -9
 cd ~/Drone-Project-code/Payload\ Computer/
