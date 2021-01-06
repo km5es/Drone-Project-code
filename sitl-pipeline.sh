ab56a1c319274f96ed6ae2bb67b9078eb0631a3b
km5es <krishnamakhija@gmail.com>
2021-01-05 22:51:22 -0500

    sitl shell script updated for ardupilot does not yet take command line arguments. flight paths now do ardupilot and/or px4 with correct heading in each.

--- sitl-pipeline.sh
+++ sitl-pipeline.sh
@@ -8,11 +8,19 @@ lsof -t -i udp:14540 | xargs kill -9
 kill -9 $(fuser /dev/ttyUSB0)
 lsof -t -i tcp:8800 | xargs kill -9
 
-### run MAVROS
-roslaunch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557" &
-
 ### run PX4 simulator in headless mode
-gnome-terminal -e "bash -c 'cd ~/src/Firmware/ && HEADLESS=1 make px4_sitl gazebo_solo;'"
+# PX4
+#gnome-terminal -e "bash -c 'cd ~/src/Firmware/ && HEADLESS=1 make px4_sitl gazebo_solo;'"
+# APM
+gnome-terminal -e "bash -c 'cd ~/ardupilot && sim_vehicle.py -L MiltonAirfield -v ArduCopter --console –map
+;'"
+
+### run MAVROS
+# PX4
+#roslaunch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557" &
+# APM
+roslaunch mavros apm.launch fcu_url:="udp://:14550@127.0.0.1:14551" &
+
 
 ### run MAVROS
 #gnome-terminal -e "bash -c 'roslaunch mavros px4.launch fcu_url:="udp://:14540@127.0.0.1:14557"'"
