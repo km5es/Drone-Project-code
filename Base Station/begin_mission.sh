66070874d70b1d4950e67a30d5892beee2db75d2
km5es <krishnamakhija@gmail.com>
2021-03-05 17:08:01 -0500

    updating begin_mission.sh for posterity and robustness.

--- Base Station/begin_mission.sh
+++ Base Station/begin_mission.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 #### A convenience script to run the entire autonomy pipeline during the experiment.
 ###### Author: KM
-######## Date: Sep 13th 2020
+######## Date: Mar 5th 2021
 
 ### clear any previously running scripts
 lsof -t -i tcp:8800 | xargs kill -9
 
-### intitate MAVLink with drone
-#roslaunch mavros apm.launch fcu_url:="/dev/ttyMAVROS:57600" &
+if [ $1 = "wifi" ]; then
+	echo "Connecting to drone via $1"
+#    roslaunch mavros apm.launch fcu_url:="udp://:15550@127.0.0.1:15551" &
+    gnome-terminal -e "bash -c 'roslaunch mavros apm.launch fcu_url:=udp://:15550@10.42.0.1:15551 gcs_url:=udp://@127.0.0.1:17550'" 
 
-#mavlink-routerd -e 127.0.0.1:15550 -e 127.0.0.1:17550 10.42.0.1:12550 &
-roslaunch mavros apm.launch fcu_url:="udp://:15550@127.0.0.1:15551" &
+elif [ $1 = "telemetry" ]; then
+	echo "Connecting to drone via $1"
+    gnome-terminal -e "bash -c 'roslaunch mavros apm.launch fcu_url:=/dev/ttyMAVROS:57600 gcs_url:=udp://@127.0.0.1:17550'"
 
-sleep 4
+else
+    echo "Connecting to drone via wifi (default)"
+    gnome-terminal -e "bash -c 'roslaunch mavros apm.launch fcu_url:=udp://:15550@10.42.0.1:15551 gcs_url:=udp://@127.0.0.1:17550'" 
+fi
 
-### begin ROS script
+sleep 6
+rosrun mavros mavsys rate --all 10
+
+#### begin ROS script
 gnome-terminal -e "bash -c 'rosrun beam_mapping ros-trigger.py;'"
 
 
-### run GRC TCP server
+#### run GRC TCP server
 gnome-terminal -e "bash -c 'python ~/catkin_ws/src/Drone-Project-code/Base\ Station/tcp_toggle.py;'"
 
-sleep 6;
+#sleep 6;
 
-### run GRC TCP client
-gnome-terminal -e "bash -c 'python ~/catkin_ws/src/Drone-Project-code/Base\ Station/base-station-receiver.py;'"
+#### run GRC TCP client
+if [ $1 = "wifi" ] || [ $1 = "telemetry"]; then
+    gnome-terminal -e "bash -c 'python ~/catkin_ws/src/Drone-Project-code/Base\ Station/base-station-receiver.py -n $1;'"
 
-### run QGroundControl
+else
+    gnome-terminal -e "bash -c 'python ~/catkin_ws/src/Drone-Project-code/Base\ Station/base-station-receiver.py;'"
+fi
+
+#### run QGroundControl
 ~/Downloads/QGroundControl.AppImage
