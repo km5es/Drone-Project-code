df6465ec354aeadbcc4ddfdd8a800582512fd4e8
km5es <krishnamakhija@gmail.com>
2021-07-18 05:09:31 -0400

    trying to detect pi on boot up

--- Payload Computer/connect_to_fc.sh
+++ Payload Computer/connect_to_fc.sh
@@ -30,6 +30,12 @@ elif echo $USER | grep -q 'pi'; then
     ### uncomment for SITL simulations
     #roslaunch mavros apm.launch fcu_url:="udp://:16550@10.42.0.1:16551" &
 
+elif uname -a | grep -q 'raspberrypi'; then
+    echo "Raspberry Pi OS detected..."
+	roslaunch mavros apm.launch fcu_url:="/dev/ttyS0:921600" &
+    ### uncomment for SITL simulations
+    #roslaunch mavros apm.launch fcu_url:="udp://:16550@10.42.0.1:16551" &
+
 else
     echo "No MAVLink connection found..."
     exit
