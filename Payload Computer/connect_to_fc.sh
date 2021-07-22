9eb45dfa48bfbd094e23732848bd83a9a2438b8a
km5es <krishnamakhija@gmail.com>
2021-07-21 20:18:59 -0400

    error with new connect to fc code

--- Payload Computer/connect_to_fc.sh
+++ Payload Computer/connect_to_fc.sh
@@ -32,7 +32,7 @@ elif echo $USER | grep -q 'pi'; then
 
 elif uname -a | grep -q 'raspberrypi'; then
     echo "Raspberry Pi OS detected..."
-    /usr/bin/python run_on_pi.py &
+    /usr/bin/python /home/pi/catkin_ws/src/Drone-Project-code/Payload\ Computer/run_on_pi.py &
 	roslaunch mavros apm.launch fcu_url:="/dev/ttyS0:921600" &
     ### uncomment for SITL simulations
     #roslaunch mavros apm.launch fcu_url:="udp://:16550@10.42.0.1:16551" &
