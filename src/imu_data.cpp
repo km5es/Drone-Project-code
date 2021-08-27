a1a4d1a827ea81a90caab1fc89d47d24a386c7cb
km5es <km5es>
2021-08-27 04:24:36 -0400

    starting to convert to c++

---
+++ src/imu_data.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+#include "nav_msgs/Odometry.h"
+#include <iostream>
+
+void chatterCallback(const sensor_msgs::Imu::ConstPtr& msg){
+
+    ROS_INFO("\nlinear acceleration\
+                \nx: [%f]\ny:[%f]\nz:[%f]\nang_x: [%f]", msg->linear_acceleration.x,
+                msg->linear_acceleration.y, msg->linear_acceleration.z, msg->angular_velocity.x);
+
+    // std::cout << msg->linear_acceleration.z << "\n";
+}
+
+
+void odom_callback(const nav_msgs::Odometry::ConstPtr& msg){
+    ROS_INFO("\nangular acceleration\nx: [%f]", msg->twist.twist.angular.x);
+}
+
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "imu_data");
+    ros::NodeHandle n;
+    ros::Subscriber sub1 = n.subscribe("/mavros/imu/data", 1000, chatterCallback);
+    ros::Subscriber sub2 = n.subscribe("/mavros/local_position/odom", 1000, odom_callback);
+    ros::spin();
+    return 0;
+}
---
+++ src/wp_trigger.cpp
@@ -0,0 +1,66 @@
+/*
+* Re-writing some of the ROS nodes in C++ for better runtime efficiency.
+* author: KM
+*/ 
+
+#include "ros/ros.h"
+#include "sensor_msgs/Imu.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "sensor_msgs/NavSatFix.h"
+#include "mavros_msgs/WaypointList.h"
+#include <iostream>
+#include <math.h>
+#include <cmath>
+#include <thread>
+
+double vel_threshold = 0.35;
+double pos_error_tol = 1.0;
+double wp_x_lat, wp_y_long, wp_z_alt;
+double h;
+
+double haversine(double lat1, double long1, double lat2, double long2){
+    double r = 6.3781e6;
+    double phi1 = lat1 * M_PI / 180;
+    double phi2 = lat2 * M_PI / 180;
+    double lam1 = long1 * M_PI / 180;
+    double lam2 = long2 * M_PI / 180;
+    double delta_phi = phi2 - phi1;
+    double delta_lam = lam2 - lam1;
+    double a = sin(delta_phi/2)*sin(delta_phi/2) + cos(lat1)*cos(lat2)*sin(delta_lam/2)*sin(delta_lam/2);
+    double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    double d = r * c;
+    return d;
+}
+
+double haversine_3d(double lat1, double long1, double alt1, double lat2, double long2, double alt2){
+    double alt_diff = alt2 - alt1;
+    double d_2d = haversine(lat1, long1, lat2, long2);
+    double d_3d = sqrt(pow(d_2d, 2) + pow(alt_diff, 2));
+    return d_3d;
+}
+
+void get_velocity(const nav_msgs::Odometry::ConstPtr& msg){
+    double x_vel = msg->twist.twist.linear.x;
+    double y_vel = msg->twist.twist.linear.y;
+    double z_vel = msg->twist.twist.linear.z;
+    double v = sqrt(pow(x_vel, 2) + pow(y_vel, 2) + pow(z_vel, 2));
+    ROS_INFO("v = %f", v);
+}
+
+void get_waypoints(const mavros_msgs::WaypointList::ConstPtr& msg){
+    //std::vector<int> wp_list = msg->waypoints;
+    //double wp_list = msg->waypoints;
+    std::cout << msg << "\n";
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "wp_trigger");
+    ros::NodeHandle n;
+    ros::Subscriber sub1 = n.subscribe("/mavros/mission/waypoints", 1000, get_waypoints);
+    //ros::Subscriber sub2 = n.subscribe("/mavros/global_position/global", 1000, get_haversine);
+    //ros::Subscriber sub3 = n.subscribe("/mavros/local_position/pose", 1000, get_distance);
+    ros::Subscriber sub4 = n.subscribe("/mavros/local_position/odom", 1000, get_velocity);
+    ros::spin();
+    return 0;
+}
