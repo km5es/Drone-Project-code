78b2e7bbf5d4858a18280cfc93e2a0d0b1e1fa5e
km5es <km5es>
2021-09-02 08:32:55 -0400

    c++ version of write_WPs also seems to be working and needs testing now

--- src/write_WPs.cpp
+++ src/write_WPs.cpp
@@ -17,6 +17,12 @@
 #include <string.h>
 #include <sys/types.h>
 #include <pwd.h>
+#include <exception>
+#include <vector>
+#include <iterator>
+#include <sstream>
+#include <iomanip>
+#include <stdexcept>      // std::out_of_range
 #include <mavros_msgs/SetMode.h>
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/Waypoint.h>
@@ -26,15 +32,42 @@
 #include <mavros_msgs/CommandHome.h>
 
 // Define global variables
+bool seq_flag, way_flag;
 struct passwd *pw = getpwuid(getuid());
 const char *homedir = pw->pw_dir;   // get path to home directory
 std::string filename = "/catkin_ws/src/Drone-Project-code/mission/mission.waypoints";
+mavros_msgs::Waypoint wp;           // WPs object
+
+//? split strings according to specified delimiter
+void split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss;
+    ss.str(s);
+    std::string item;
+    while (std::getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+//? toggle modes before departing each WP. The WP table needs to be cleared 
+//? before updating with a new WP and switching "back" to Auto.
+void change_mode(){
+    ros::NodeHandle l;
+    ros::ServiceClient change_mode_client = 
+                            l.serviceClient<mavros_msgs::SetMode>("/mavros/set_mode");
+    mavros_msgs::SetMode brake_set_mode;
+    mavros_msgs::SetMode auto_set_mode;
+    brake_set_mode.request.custom_mode = "BRAKE";
+    auto_set_mode.request.custom_mode = "AUTO";
+    change_mode_client.call(brake_set_mode);
+    change_mode_client.call(auto_set_mode);
+    ROS_INFO("Toggling modes on copter.");
+}
 
 //? clear all waypoints on the FCU
 void waypoint_clear_client(){
-    ros::NodeHandle p;  
+    ros::NodeHandle m;  
     ros::ServiceClient wp_clear_client = 
-                p.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear");
+                m.serviceClient<mavros_msgs::WaypointClear>("/mavros/mission/clear");
     mavros_msgs::WaypointClear wp_clear_srv;
     wp_clear_srv.request = {};
     if (wp_clear_client.call(wp_clear_srv)){
@@ -45,16 +78,14 @@ void waypoint_clear_client(){
     }
 }
 
-/*
-? Create a waypoint object to be sent to the FC.
-?    Command: tells the drone what to do (navigate, land, RTL, etc)
-?        22: takeoff
-?        115: yaw
-?        16: navigate to WP
-?        19: loiter
-?        20: RTL
-?    param1: how to do it (delay at WP, yaw angle)
-*/
+//? Create a waypoint object to be sent to the FC.
+//?    Command: tells the drone what to do (navigate, land, RTL, etc)
+//?        22: takeoff
+//?        115: yaw
+//?        16: navigate to WP
+//?        19: loiter
+//?        20: RTL
+//?    param1: how to do it (delay at WP, yaw angle)
 mavros_msgs::Waypoint create_waypoint(int command, double param1, double latitude, 
                                                 double longitude, double altitude){
     mavros_msgs::Waypoint wp_msg;
@@ -73,8 +104,74 @@ mavros_msgs::Waypoint create_waypoint(int command, double param1, double latitud
 }
 
 int main(int argc, char **argv){
+    // ? arrange data from mission.waypoints into separate vectors.
+    //? this seems goofy. maybe there is a better way to do this?
     std::string &filenameptr = filename;
     filenameptr = homedir + filenameptr;
+    std::ifstream mission_file(filename);                       // load mission files
+    std::string line;
+    std::vector<double> command, param1, latitude, longitude, altitude;
+    if (mission_file.is_open()){
+        ROS_INFO("Mission file retrieved from: %s", filename.c_str());
+    }
+    else {
+        ROS_ERROR("Failed to open mission file.");
+    }
+    std::getline(mission_file, line);                           // skip the first line
+    while (std::getline(mission_file, line)){                   // while each line is read
+        std::vector<std::string> data_per_row;
+        split(line, '\t', data_per_row);                        // each line is separate entry
+        for (auto v: data_per_row)
+            //std::cout << v << "\t" ;
+            //std::cout << std::endl;;
+            command.push_back(std::stod(data_per_row[3]));
+            param1.push_back(std::stod(data_per_row[4]));       // creating separate vectors 
+            latitude.push_back(std::stod(data_per_row[8]));     //   for separate args to 
+            longitude.push_back(std::stod(data_per_row[9]));    //      create_waypoint()
+            altitude.push_back(std::stod(data_per_row[10]));
+    }
     ros::init(argc, argv, "write_WP");
+    ros::NodeHandle nh;
+    ros::ServiceClient wp_client = 
+                nh.serviceClient<mavros_msgs::WaypointPush>("mavros/mission/push");
+    mavros_msgs::WaypointPush wp_push_srv;          // List of Waypoints
+    mavros_msgs::Waypoint wp;                       // WPs object
     waypoint_clear_client();
+    int n = 1;                                      // wp counters
+    //? create initial WP on bootup
+    wp = create_waypoint(22, 0, latitude[n], longitude[n], altitude[n]);
+    wp_push_srv.request.waypoints.push_back(wp);
+    wp = create_waypoint(115, param1[n+1], latitude[n], longitude[n], altitude[n]);
+    wp_push_srv.request.waypoints.push_back(wp);
+    wp = create_waypoint(16, 0, latitude[n], longitude[n], altitude[n]);
+    wp_push_srv.request.waypoints.push_back(wp);
+    wp_client.call(wp_push_srv);                    // this pushes WP list to FCU
+    while (ros::ok()){
+        usleep(5e4);
+        if (nh.param("trigger/sequence", seq_flag) == true){
+            waypoint_clear_client();
+        }
+        if (nh.param("trigger/waypoint", seq_flag) == true){
+            nh.setParam("trigger/waypoint", false);
+            ROS_INFO("Updating WP table.");
+            waypoint_clear_client();                                // ! remove this later
+            n = n + 2;
+            if (n <= altitude.size() - 2){
+                wp = create_waypoint(22, 0, latitude[n], longitude[n], altitude[n]);
+                wp_push_srv.request.waypoints.push_back(wp);
+                wp = create_waypoint(115, param1[n+1], latitude[n], longitude[n], altitude[n]);
+                wp_push_srv.request.waypoints.push_back(wp);
+                wp = create_waypoint(16, 0, latitude[n], longitude[n], altitude[n]);
+                wp_push_srv.request.waypoints.push_back(wp);
+                wp_client.call(wp_push_srv);
+            }
+            else {
+                ROS_INFO("End of WP table reached. Doing an RTL now.");
+                wp = create_waypoint(20, 0, 0, 0, 0);               // ! double check this in SITL
+                wp_push_srv.request.waypoints.push_back(wp);
+                wp_client.call(wp_push_srv);
+            }
+        }
+    }
+    return 0;
 }
