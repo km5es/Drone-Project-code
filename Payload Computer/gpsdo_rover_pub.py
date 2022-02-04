6ea26e850be1cf194f9b760c3a34adcec5e70baa
km5es <krishnamakhija@gmail.com>
2022-02-03 20:36:58 -0500

    convert indentation to spaces what is this now?

--- Payload Computer/gpsdo_rover_pub.py
+++ Payload Computer/gpsdo_rover_pub.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Create ROS talker node to retrieve GPSDO systematics and publish them.
 These are the systematics being published:
@@ -26,60 +27,60 @@ rm = visa.ResourceManager()
 print(rm.list_resources())
 
 try:
-	gpsdo_base = rm.open_resource('ASRL/dev/ttyUSB0::INSTR', baud_rate=115200, 
-		data_bits=8, parity=Parity.none, stop_bits=StopBits.one, timeout=3000)
-	gpsdo_base.set_visa_attribute(constants.VI_ATTR_ASRL_FLOW_CNTRL, constants.VI_ASRL_FLOW_NONE)
-	gpsdo_base.write_termination = "\n"
-	gpsdo_base.read_termination = "\n"
-	### Make sure the base is connected
-	gpsdo_base.query('*IDN?')
-	idn = gpsdo_base.read_bytes(50)
-	if 'Fury' in idn.decode("utf-8"):
-		print(colored("GPSDO base (Fury Firmware Rev 1.23) detected. Starting GPSDO ROS node now.", 'green'))
-	else:
-		print(colored("Unit detected: %s", idn, 'red'))
+    gpsdo_base = rm.open_resource('ASRL/dev/ttyUSB0::INSTR', baud_rate=115200, 
+        data_bits=8, parity=Parity.none, stop_bits=StopBits.one, timeout=3000)
+    gpsdo_base.set_visa_attribute(constants.VI_ATTR_ASRL_FLOW_CNTRL, constants.VI_ASRL_FLOW_NONE)
+    gpsdo_base.write_termination = "\n"
+    gpsdo_base.read_termination = "\n"
+    ### Make sure the base is connected
+    gpsdo_base.query('*IDN?')
+    idn = gpsdo_base.read_bytes(50)
+    if 'Fury' in idn.decode("utf-8"):
+        print(colored("GPSDO base (Fury Firmware Rev 1.23) detected. Starting GPSDO ROS node now.", 'green'))
+    else:
+        print(colored("Unit detected: %s", idn, 'red'))
 except:
-	print("No GPSDO detected. Program will be terminated.")
-		
+    print("No GPSDO detected. Program will be terminated.")
+        
 
 
 class gpsdo_data:
-	def __init__(self):
-		self.data = []
+    def __init__(self):
+        self.data = []
 
-	def idn(self):
-		gpsdo_base.query('*IDN?')
-		idn = gpsdo_base.read_bytes(40).decode("utf-8")
-		return idn
+    def idn(self):
+        gpsdo_base.query('*IDN?')
+        idn = gpsdo_base.read_bytes(40).decode("utf-8")
+        return idn
 
-	def fee(self):
-		gpsdo_base.query('SYNC:FEE?')
-		fee = gpsdo_base.read_bytes(16).decode("utf-8")
-		return fee[:8]
+    def fee(self):
+        gpsdo_base.query('SYNC:FEE?')
+        fee = gpsdo_base.read_bytes(16).decode("utf-8")
+        return fee[:8]
 
-	def lock(self):
-		gpsdo_base.query('SYNC:LOCK?')
-		lock = gpsdo_base.read_bytes(2).decode("utf-8")
-		return lock[:1]
+    def lock(self):
+        gpsdo_base.query('SYNC:LOCK?')
+        lock = gpsdo_base.read_bytes(2).decode("utf-8")
+        return lock[:1]
 
-	def time(self):
-		gpsdo_base.query('PTIM:TIME:STR?')
-		time = gpsdo_base.read_bytes(16).decode("utf-8")
-		return time[:8]
+    def time(self):
+        gpsdo_base.query('PTIM:TIME:STR?')
+        time = gpsdo_base.read_bytes(16).decode("utf-8")
+        return time[:8]
 
-	def tint(self):
-		gpsdo_base.query('SYNC:TINT?')
-		tint = gpsdo_base.read_bytes(16).decode("utf-8")
-		return tint[:8]
+    def tint(self):
+        gpsdo_base.query('SYNC:TINT?')
+        tint = gpsdo_base.read_bytes(16).decode("utf-8")
+        return tint[:8]
 
 
 def talker():
-	pub = rospy.Publisher('gpsdo_base/fee', String, queue_size=1)
-	rospy.init_node('gpsdo_base_node', anonymous=True)
-	rate = rospy.Rate(4) # 2 Hz
-	data = gpsdo_data()
-	while not rospy.is_shutdown():
-		rospy.loginfo(data.fee() + '\t' + data.time())
+    pub = rospy.Publisher('gpsdo_base/fee', String, queue_size=1)
+    rospy.init_node('gpsdo_base_node', anonymous=True)
+    rate = rospy.Rate(4) # 2 Hz
+    data = gpsdo_data()
+    while not rospy.is_shutdown():
+        rospy.loginfo(data.fee() + '\t' + data.time())
         pub.publish(data.fee() + '\t' + data.time())
         rate.sleep()
 
