628024817510e90f1a568733ac9925f0830c07b0
km5es <km5es>
2022-02-12 13:45:43 -0500

    reverting changes to previous

--- Payload Computer/top_block.py
+++ Payload Computer/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Sat Feb 12 12:54:11 2022
+# Generated: Sat Feb 12 13:31:47 2022
 ##################################################
 
 if __name__ == '__main__':
@@ -17,14 +17,16 @@ if __name__ == '__main__':
             print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
-from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
+from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import numpy as np
+import pmt
+import sip
 import sys
 from gnuradio import qtgui
 
@@ -71,28 +73,60 @@ class top_block(gr.top_block, Qt.QWidget):
         ##################################################
         # Blocks
         ##################################################
-        self.blocks_vector_source_x_0 = blocks.vector_source_c(np.hstack((np.zeros(OFF), np.ones(ON))), True, 1, [])
-        (self.blocks_vector_source_x_0).set_min_output_buffer(4096)
-        (self.blocks_vector_source_x_0).set_max_output_buffer(4096)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
-        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
-        (self.blocks_multiply_xx_0).set_min_output_buffer(4096)
-        (self.blocks_multiply_xx_0).set_max_output_buffer(4096)
-        self.blocks_head_0 = blocks.head(gr.sizeof_gr_complex*1, head)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '/home/kmakhija/catkin_ws/src/Drone-Project-code/Payload Computer/noise', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 0.5, 42)
+        self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
+        	4096, #size
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_freq_sink_x_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0.set_y_axis(-140, 10)
+        self.qtgui_freq_sink_x_0.set_y_label('Relative Gain', 'dB')
+        self.qtgui_freq_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0.enable_grid(False)
+        self.qtgui_freq_sink_x_0.set_fft_average(1.0)
+        self.qtgui_freq_sink_x_0.enable_axis_labels(True)
+        self.qtgui_freq_sink_x_0.enable_control_panel(False)
+
+        if not True:
+          self.qtgui_freq_sink_x_0.disable_legend()
+
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_freq_sink_x_0.set_plot_pos_half(not True)
+
+        labels = ['', '', '', '', '',
+                  '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
+        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_gr_complex*1, samp_rate,True)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_gr_complex*1, '/home/kmakhija/catkin_ws/src/Drone-Project-code/Payload Computer/noise', True)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_multiply_xx_0, 0))
-        self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_multiply_xx_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_head_0, 0))
-        self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_multiply_xx_0, 1))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0_0, 0))
+        self.connect((self.blocks_throttle_0_0, 0), (self.qtgui_freq_sink_x_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
@@ -104,17 +138,17 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_ON(self, ON):
         self.ON = ON
-        self.set_OFF(15*self.ON)
         self.set_duty_cycle(self.OFF/self.ON)
-        self.blocks_vector_source_x_0.set_data(np.hstack((np.zeros(self.OFF), np.ones(self.ON))), [])
         self.set_ON_time(self.ON/self.samp_rate)
+        self.set_OFF(15*self.ON)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.blocks_throttle_0_0.set_sample_rate(self.samp_rate)
         self.set_ON_time(self.ON/self.samp_rate)
         self.set_OFF_time(self.OFF/self.samp_rate)
 
@@ -124,7 +158,6 @@ class top_block(gr.top_block, Qt.QWidget):
     def set_OFF(self, OFF):
         self.OFF = OFF
         self.set_duty_cycle(self.OFF/self.ON)
-        self.blocks_vector_source_x_0.set_data(np.hstack((np.zeros(self.OFF), np.ones(self.ON))), [])
         self.set_OFF_time(self.OFF/self.samp_rate)
 
     def get_ring_buffer_size(self):
@@ -138,7 +171,6 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_head(self, head):
         self.head = head
-        self.blocks_head_0.set_length(self.head)
 
     def get_duty_cycle(self):
         return self.duty_cycle
