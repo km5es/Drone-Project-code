02b76f7f78b12fbecbd96fa9c6c71874b8fb0b33
km5es <krishnamakhija@gmail.com>
2022-02-16 20:47:19 -0500

    now I have underflows. why?

--- Payload Computer/zeros.py
+++ Payload Computer/zeros.py
@@ -3,20 +3,9 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Zeros
-# Generated: Wed Feb 16 15:49:52 2022
+# Generated: Wed Feb 16 20:46:25 2022
 ##################################################
 
-if __name__ == '__main__':
-    import ctypes
-    import sys
-    if sys.platform.startswith('linux'):
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-
-from PyQt4 import Qt
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import gr
@@ -24,42 +13,18 @@ from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
 import numpy as np
-import sys
-from gnuradio import qtgui
 
 
-class zeros(gr.top_block, Qt.QWidget):
+class zeros(gr.top_block):
 
     def __init__(self):
         gr.top_block.__init__(self, "Zeros")
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Zeros")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except:
-            pass
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "zeros")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
-
 
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 15.36e6/2
-        self.head = head = 4096*16
+        self.head = head = 8200*512
 
         ##################################################
         # Blocks
@@ -79,11 +44,6 @@ class zeros(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_throttle_0, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.blocks_vector_source_x_0, 0), (self.blocks_head_0_0_0, 0))
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "zeros")
-        self.settings.setValue("geometry", self.saveGeometry())
-        event.accept()
-
     def get_samp_rate(self):
         return self.samp_rate
 
@@ -102,21 +62,9 @@ class zeros(gr.top_block, Qt.QWidget):
 
 def main(top_block_cls=zeros, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
-        style = gr.prefs().get_string('qtgui', 'style', 'raster')
-        Qt.QApplication.setGraphicsSystem(style)
-    qapp = Qt.QApplication(sys.argv)
-
     tb = top_block_cls()
     tb.start()
-    tb.show()
-
-    def quitting():
-        tb.stop()
-        tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
-    qapp.exec_()
+    tb.wait()
 
 
 if __name__ == '__main__':
