72b050c87a5ef92ca9dca640c62392ceffa2105d
km5es <krishnamakhija@gmail.com>
2022-04-05 02:28:36 -0400

    migrated gr_cal to python3. how much damage will this do?

--- Payload Computer/gr_cal_tcp_loopback_client.py
+++ Payload Computer/gr_cal_tcp_loopback_client.py
@@ -1,28 +1,29 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-##################################################
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
 # GNU Radio Python Flow Graph
 # Title: gr_cal_tcp_loopback_client
 # Author: KM
 # Description: This will go on the drone. A predefined waveform is fed into the companion script which creates a TCP server and loops back into this script. The server also checks for serial toggle and triggers GPIO at set points.
-# Generated: Wed Feb 16 22:24:03 2022
-##################################################
+# GNU Radio version: 3.8.1.0
 
 from gnuradio import blocks
-from gnuradio import eng_notation
 from gnuradio import gr
-from gnuradio import uhd
-from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
-from grc_gnuradio import blks2 as grc_blks2
-from optparse import OptionParser
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import uhd
 import time
-import rospy
-from std_msgs.msg import Float32
 
 class gr_cal_tcp_loopback_client(gr.top_block):
 
-    def __init__(self, device_transport='send_frame_size=8192,  num_send_frames=512'):
+    def __init__(self, device_transport=send_frame_size=8192,  num_send_frames=512):
         gr.top_block.__init__(self, "gr_cal_tcp_loopback_client")
 
         ##################################################
@@ -37,40 +38,34 @@ class gr_cal_tcp_loopback_client(gr.top_block):
         self.wave_freq = wave_freq = samp_rate/8
         self.meas_freq = meas_freq = 150e6
         self.min_buffer = min_buffer = 65536
-        self.gain = gain = 40
+        self.gain = gain = 60
         self.freq = freq = meas_freq - wave_freq
 
         ##################################################
         # Blocks
         ##################################################
         self.uhd_usrp_sink_0 = uhd.usrp_sink(
-        	",".join(("", "")),
-        	uhd.stream_args(
-        		cpu_format="fc32",
-        		channels=range(1),
-        	),
+            ",".join(("", "")),
+            uhd.stream_args(
+                cpu_format="fc32",
+                args='',
+                channels=list(range(0,1)),
+            ),
+            '',
         )
         self.uhd_usrp_sink_0.set_clock_source('external', 0)
-        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
         self.uhd_usrp_sink_0.set_center_freq(freq, 0)
         self.uhd_usrp_sink_0.set_gain(gain, 0)
-        self.blocks_vector_to_stream_0 = blocks.vector_to_stream(gr.sizeof_gr_complex*1, min_buffer)
-        (self.blocks_vector_to_stream_0).set_min_output_buffer(65536)
-        self.blks2_tcp_source_0 = grc_blks2.tcp_source(
-        	itemsize=gr.sizeof_gr_complex*min_buffer,
-        	addr='127.0.0.1',
-        	port=8810,
-        	server=False,
-        )
-        (self.blks2_tcp_source_0).set_min_output_buffer(65536)
+        self.uhd_usrp_sink_0.set_samp_rate(samp_rate)
+        self.uhd_usrp_sink_0.set_time_unknown_pps(uhd.time_spec())
+        self.blocks_udp_source_0 = blocks.udp_source(gr.sizeof_gr_complex*1, '127.0.0.1', 8810, 1472, True)
 
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blks2_tcp_source_0, 0), (self.blocks_vector_to_stream_0, 0))
-        self.connect((self.blocks_vector_to_stream_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.blocks_udp_source_0, 0), (self.uhd_usrp_sink_0, 0))
 
     def get_device_transport(self):
         return self.device_transport
@@ -113,7 +108,6 @@ class gr_cal_tcp_loopback_client(gr.top_block):
         self.gain = gain
         self.uhd_usrp_sink_0.set_gain(self.gain, 0)
 
-
     def get_freq(self):
         return self.freq
 
@@ -121,36 +115,29 @@ class gr_cal_tcp_loopback_client(gr.top_block):
         self.freq = freq
         self.uhd_usrp_sink_0.set_center_freq(self.freq, 0)
 
-    def get_temp(self):
-        return self.uhd_usrp_sink_0.get_sensor('temp').to_real()
-
 
 def argument_parser():
     description = 'This will go on the drone. A predefined waveform is fed into the companion script which creates a TCP server and loops back into this script. The server also checks for serial toggle and triggers GPIO at set points.'
-    parser = OptionParser(usage="%prog: [options]", option_class=eng_option, description=description)
-    parser.add_option(
-        "", "--device-transport", dest="device_transport", type="string", default='send_frame_size=4096, num_send_frames=512',
-        help="Set device_transport [default=%default]")
+    parser = ArgumentParser(description=description)
     return parser
 
 
 def main(top_block_cls=gr_cal_tcp_loopback_client, options=None):
     if options is None:
-        options, _ = argument_parser().parse_args()
+        options = argument_parser().parse_args()
     if gr.enable_realtime_scheduling() != gr.RT_OK:
-        print "Error: failed to enable real-time scheduling."
-    
-    pub = rospy.Publisher('sdr_temperature', Float32, queue_size=10)
-    rospy.init_node('SDR_temperature_node', anonymous=True)
-    rate = rospy.Rate(5) # 5 Hz
+        print("Error: failed to enable real-time scheduling.")
+    tb = top_block_cls()
 
-    tb = top_block_cls(device_transport=options.device_transport)
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
     tb.start()
-    while not rospy.is_shutdown():
-        temp = tb.get_temp()
-#        rospy.loginfo(temp)
-        pub.publish(temp)
-        rate.sleep()
     tb.wait()
 
 
