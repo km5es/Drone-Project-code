79fa9a39aeb5d565611978f610398112a4b48889
km5es <krishnamakhija@gmail.com>
2023-05-09 20:46:24 -0400

    new branch wherein I have patched in no_telem shell scripts into the phase_meas branch. hopefully this branch will be phase coherent and be able to do more accurate amplitude measurements as well using sdr temp data.

--- Payload Computer/connect_to_fc.sh
+++ Payload Computer/connect_to_fc.sh
@@ -4,6 +4,7 @@
 ###### date: Mar 5th 2021
 source ~/.bashrc
 source /opt/ros/melodic/setup.bash
+source ~/catkin_ws/devel/setup.bash
 
 ### wait for FC to boot up
 sleep 10;
@@ -20,14 +21,14 @@ if ls /dev/ttySAC0 | grep -q 'ttySAC0'; then
 elif ls /dev/ttyFC | grep -q 'ttyFC'; then
     echo "FTDI adapter detected..."
     #mavlink-routerd -e 10.42.0.1:15550 -e 127.0.0.1:14550 /dev/ttyFC:921600 &
-    roslaunch mavros apm.launch fcu_url:="/dev/ttyFC:921600" &
+    roslaunch mavros apm.launch fcu_url:="/dev/ttyFC:57600" &
     ### uncomment for SITL simulations
     #roslaunch mavros apm.launch fcu_url:="udp://:16550@10.42.0.1:16551" &
 
 elif uname -a | grep -q 'raspberrypi'; then
     echo "Raspberry Pi OS detected..."
     /usr/bin/python /home/pi/catkin_ws/src/Drone-Project-code/Payload\ Computer/run_on_pi.py &
-    roslaunch mavros apm.launch fcu_url:="/dev/ttyS0:921600" &
+    roslaunch mavros apm.launch fcu_url:="/dev/ttyS0:57600" &
     ### uncomment for SITL simulations
     #roslaunch mavros apm.launch fcu_url:="udp://:16550@10.42.0.1:16551" &
 
@@ -42,6 +43,7 @@ fi
 
 sleep 8;
 
-rosrun mavros mavsys rate --all 10 &
-
+rosrun mavros mavsys rate --all 40 &
+#rosservice call /mavros/set_message_interval 32 40
+#rosservice call /mavros/set_message_interval 33 40 
 
--- Payload Computer/start_cal_v2.sh
+++ Payload Computer/start_cal_v2.sh
@@ -8,7 +8,7 @@ lsof -t -i tcp:8810 | xargs kill -9
 cd ~/catkin_ws/src/Drone-Project-code/Payload\ Computer/
 
 #<< Python_version
-sleep 35        # wait for connection to wifi hotspot
+sleep 40        # wait for connection to wifi hotspot
 /usr/bin/python cal_sequence_tcp_server_v2.py &
 
 sleep 1
